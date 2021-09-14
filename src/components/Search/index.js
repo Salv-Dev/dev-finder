dd16451ab54e7c9687c02392629bbf2ded80b1a3
Salv-Dev <salv.dev.br@gmail.com>
2021-09-13 21:04:19 -0300

    search component styles

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,12 +3,16 @@ import { FiSearch } from 'react-icons/fi';
 
 function SearchComponent() {
   return (
-      <div>
-          <FiSearch />
-          <input type="text" placeholder="Procure por username..." />
-          <button>
-              <p>Pesquisar</p>
-          </button>
+      <div className="search">
+          <div className="search__icon">
+            <FiSearch size={26} color="#0279fb" style={{ background: 'transparent' }}/>
+          </div>
+          <div className="search__form">
+            <input className="search__form__input" type="text" placeholder="Procure por username..." />
+            <button className="search__form__button">
+                <p className="search__form__button__text">Pesquisar</p>
+            </button>
+          </div>
       </div>
   );
 }

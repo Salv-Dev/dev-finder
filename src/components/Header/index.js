2a1432e0d3bd81a64360d1f8b4263a6ce3b9ac3e
Salv-Dev <salv.dev.br@gmail.com>
2021-09-19 14:20:41 -0300

    README

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { HiSun } from 'react-icons/hi';
 function HeaderComponent() {
   return (
     <header className="header">
-        <h1 className="header__title"><a href="#">devfinder</a></h1>
+        <h1 className="header__title"><a href="https://www.github.com/salv-dev/dev-finder" rel="noreferrer" target="_blank" >devfinder</a></h1>
         <button className="header__themeButton">
             <p>CLARO</p>
             <HiSun color="#fff" size={22} />
